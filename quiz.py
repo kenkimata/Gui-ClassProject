cdf41900b982580f7c5d16d71f4959604980ffef
kenkimata <21082@students.mrgs.school.nz>
2024-03-12 09:51:37 +1300

    quiz

--- quiz.py
+++ quiz.py
@@ -1,9 +1,13 @@
 from tkinter import *
 
+class Quizstarter:
+    def __init__ (self,parent):
+        background_colour="yellow2"
+#frame set up
 
-class QuizStarter:
-    def _init_(self):
 
-    if __name__ == "__main__":
-        root = Tk()
-        root.title("NZ Road Rules Quiz")
+
+#*********************************Starting point of program********************#
+if __name__ == "__main__":
+    #root.title("NZ Road Rules Quiz") #instantiation, making an instance of the class Quizstarter to create the fram with its widgets, passing root as
+    #quizstarter_object = Quizstarter(root) #
