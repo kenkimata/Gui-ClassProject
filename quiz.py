5d31a7d17b7566f2bb12a9b74c329b43c89539c3
kenkimata <21082@students.mrgs.school.nz>
2024-03-15 11:37:07 +1300

    new changes

--- quiz.py
+++ quiz.py
@@ -1,13 +1,37 @@
 from tkinter import *
 
+
 class Quizstarter:
     def __init__ (self,parent):
         background_colour="yellow2"
-#frame set up
+        #frame set up
+        self.quiz_frame = Frame(parent, bg = background_colour, padx=100, pady=100)
+        self.quiz_frame.grid()
+
+        #Label widget for our heading
+        self.heading_label = Label (self.quiz_frame, text = "NZ Road Rules", font=("Tw Cen MT", "bold"), bg=background_colour)
+        self.heading_label.grid(row=0)
+
+        #Label for user name prompt
+        self.user_label = Label ( self.quiz_frame, text="Please enter your name below", font=("Tw Cen Mt", "16"), bg=background_colour)
+        self.user_label.grid(row=1)
+
+        #users input is take by Entry widget
+        self.entry_box=Entry(self.quiz_frame)
+        self.entry_box.grid(row=2, pady=20)
+        
+        #continue button
+        self.continue_button = Button (self.quiz_frame, text="Continue", bg="pink", command=self.name_collection)
+        self.continue_button.grid(row=3, pady=20)
+
+        def
+
 
 
 
 #*********************************Starting point of program********************#
 if __name__ == "__main__":
-    #root.title("NZ Road Rules Quiz") #instantiation, making an instance of the class Quizstarter to create the fram with its widgets, passing root as
-    #quizstarter_object = Quizstarter(root) #
+    root = Tk()
+    root.title("NZ Road Rules Quiz")
+    #quizstarter_object = Quizstarter(root) #inisiation, making an instance of the Classquiz starter to create
+    root.mainloop()#so the window dont close
